1bfa449bdca1f8eb26122c76b26029ef286cac7a
ASHWIN RAI <raiashwin2606@gmail.com>
2026-08-17 22:57:04 +0530

    Day 6: Q11 even or odd, Q12 positive negative or zero

---
+++ Day06/Q12_positive_negative_zero.c
@@ -0,0 +1,25 @@
+/*
+Write a program to input an integer and check whether it is positive,
+negative or zero using nested if-else.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    if (num > 0) {
+        printf("%d is Positive\n", num);
+    } else {
+        if (num < 0) {
+            printf("%d is Negative\n", num);
+        } else {
+            printf("%d is Zero\n", num);
+        }
+    }
+
+    return 0;
+}
